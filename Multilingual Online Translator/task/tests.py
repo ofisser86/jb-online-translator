89cb7293fe9a2148eab86027c76b32d2cccd2d8b
Didi <ofisser86@gmail.com>
2020-10-21 13:31:12 +0300

    Complete Stage 5/7

--- Multilingual Online Translator/task/tests.py
+++ Multilingual Online Translator/task/tests.py
@@ -3,8 +3,11 @@ from hstest.test_case import TestCase
 from hstest.check_result import CheckResult
 
 import sys
+import os
+
 if sys.platform.startswith("win"):
     import _locale
+
     # pylint: disable=protected-access
     _locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 
@@ -14,13 +17,34 @@ CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
 class TranslatorTest(StageTest):
     def generate(self):
-        return [TestCase(stdin='3\n4\nhello')]
+        return [TestCase(stdin='3\n0\nhello')]
 
     def check(self, reply, attach):
         reply = reply.lower()
-        if not ('arabic' in reply and 'turkish' in reply
-                and 'spanish translations' in reply and 'hola' in reply):
-            return CheckResult.wrong('Try to print out all the languages that your translator can translate.')
+
+        if 'arabic translations' not in reply:
+            return CheckResult.wrong("There are no \'Arabic translations\' in your output!")
+        if 'arabic examples' not in reply:
+            return CheckResult.wrong("There are no \'Arabic examples\' in your output!")
+        if 'russian translations' not in reply:
+            return CheckResult.wrong("There are no \'Russian translations\' in your output!")
+        if 'russian examples' not in reply:
+            return CheckResult.wrong("There are no \'Russian examples\' in your output!")
+
+        file_name = 'hello.txt'
+
+        if not os.path.exists(file_name):
+            return CheckResult.wrong(
+                "Looks like you didn't create a file named WORD.txt where WORD is word you want to translate.")
+
+        with open(file_name) as file:
+            output = file.read().lower()
+            if 'spanish translation' not in output:
+                return CheckResult.wrong("There are no \'Spanish translations\' in the output file!")
+            if 'spanish example' not in output:
+                return CheckResult.wrong("There are no \'Spanish examples\' in the output file!")
+            if 'turkish example' not in output:
+                return CheckResult.wrong("There are no \'Turkish examples\' in the output file!")
 
         return CheckResult.correct()
 
--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 class Translator:
     def __init__(self):
         self.LANGUAGES = {
+            '0': 'all',
             '1': 'Arabic',
             '2': 'German',
             '3': 'English',
@@ -21,6 +22,9 @@ class Translator:
         }
         self.base_url = 'https://context.reverso.net/translation'
         self.headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'}
+        self.clean_translations = None
+        self.clean_examples = None
+        self.context = None
 
     def menu(self):
         print("Hello, you're welcome to the translator. Translator supports:")
@@ -28,38 +32,53 @@ class Translator:
             print(k + '.', v)
 
         # Context include word to translate and language itself instead of language code
-        context = {'native_lang': self.LANGUAGES[input('Type the number of your language:\n')],
-                   'lang': self.LANGUAGES[input('Type the number of language you want to translate to:\n')],
-                   'word': input('Type the number of your language:\n')}
+        context = {'from_lang': self.LANGUAGES[input('Type the number of your language:\n')],
+                   'to_lang': self.LANGUAGES[input("Type the number of language you want to translate to or '0' to "
+                                                   "translate to all languages:\n")],
+                   'word': input('Type the word you want to translate:\n')}
 
-        response = self.context_translate(context)
-        return self.print_result(response, context)
+        self.context = context
+        self.write_to_file()
+        self.read_from_file()
 
+        return None
+
+    # make a request to server and get response with raw_data
     def context_translate(self, context):
-        url = f"{self.base_url}/{context['native_lang'].lower()}-{context['lang'].lower()}/{context['word']}"
+        url = f"{self.base_url}/{context['from_lang'].lower()}-{context['to_lang'].lower()}/{context['word']}"
         response = requests.get(url, headers=self.headers)
-        return response
+        self.cleaning_data(response)
+        return None
 
-    def print_result(self, content, context):
+    # cleaning raw data
+    def cleaning_data(self, content):
         raw_page = content.content
         soup = BeautifulSoup(raw_page, "html.parser")
-
-        translations_words = soup.find('div', {'id': 'translations-content'}).text.split()
-        print(f"\n{context['lang']} Translations:")
-        for i in translations_words[:5]:
-            print(i)
-
-        print(f"\n{context['lang']} Examples:")
-
+        self.clean_translations = soup.find('div', {'id': 'translations-content'}).text.split()
         raw_translate_examples = soup.find('section', {'id': 'examples-content'}).text.split('\n')
-        translate_examples = [i.strip() for i in raw_translate_examples if i][:10]
-
-        for i in range(len(translate_examples)):
-            if i % 2 == 0:
-                print(translate_examples[i] + ':')
-            else:
-                print(translate_examples[i] + '\n')
+        self.clean_examples = [i.strip() for i in raw_translate_examples if i][:10]
         return None
+
+    # Create file and write to it all scrapped data
+    def write_to_file(self):
+        # Prepare target list languages, protect from translation - from_lang-from_lang (english-english) and delete
+        # "all" from LANGUAGE
+        lang_list = [self.context['to_lang']]
+        if self.context['to_lang'] == 'all':
+            lang_list = [lang for lang in self.LANGUAGES.values() if lang not in ['all', self.context['from_lang']]]
+        with open(f"{self.context['word']}.txt", 'w') as f:
+            for language in lang_list:
+                self.context['to_lang'] = language
+                self.context_translate(context=self.context)
+                f.writelines(f"{language} Translations:" + "\n" + self.clean_translations[0] + "\n\n" +
+                             f"{language} examples:" + "\n" + self.clean_examples[0] +
+                             "\n" + self.clean_examples[1] + "\n\n")
+            f.close()
+
+    # Print the result from file
+    def read_from_file(self):
+        with open(f"{self.context['word']}.txt", 'r') as f:
+            print(f.read())
 
 
 if __name__ == "__main__":
